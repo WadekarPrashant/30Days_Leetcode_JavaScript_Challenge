c27f4bfd4355d50399a691355c4ef980d4f2f4c3
WadekarPrashant <wadekarprashant733@gmail.com>
2023-05-09 20:12:20 +0530

    Day5.js

---
+++ Day5.js
@@ -0,0 +1,10 @@
+var filter = function(arr, fn) {
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+      if (fn(arr[i], i)) {
+        result.push(arr[i]);
+      }
+    }
+    return result;
+  };
+  

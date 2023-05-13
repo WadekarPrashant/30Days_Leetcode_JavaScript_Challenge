1e3aed5417310b70197f694e187905ba3df7bdcf
WadekarPrashant <wadekarprashant733@gmail.com>
2023-05-13 20:12:20 +0530

    Day9.js

---
+++ Day9.js
@@ -0,0 +1,16 @@
+function memoize(fn) {
+    const cache = new Map();
+  
+    return function(...args) {
+      const key = JSON.stringify(args);
+  
+      if (cache.has(key)) {
+        return cache.get(key);
+      }
+  
+      const result = fn(...args);
+      cache.set(key, result);
+      return result;
+    };
+  }
+  
